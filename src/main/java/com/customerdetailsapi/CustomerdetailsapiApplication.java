7e96f296c2adbc5eab482a93516ccfa29c903db4
Krishna Yadav <90216712+krishna-yadav0@users.noreply.github.com>
2023-12-23 10:59:56 +0530

    Created customer details repository

---
+++ src/main/java/com/customerdetailsapi/CustomerdetailsapiApplication.java
@@ -0,0 +1,13 @@
+package com.customerdetailsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerdetailsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerdetailsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/customerdetailsapi/CustomerdetailsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customerdetailsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerdetailsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
